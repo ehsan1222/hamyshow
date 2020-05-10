12fb5793f387d9ee58ccaef9501d293b29b739d5
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-05-11 03:17:12 +0430

    Application permissions

---
+++ src/main/java/ir/hamyiar/security/security/auth/ApplicationPermissions.java
@@ -0,0 +1,20 @@
+package ir.hamyiar.security.security.auth;
+
+public enum ApplicationPermissions {
+    USER_READ("user:read"),
+    USER_WRITE("user:write"),
+    COMMENT_WRITE("comment:write"),
+    COMMENT_CHANGE("comment:change"),
+    MOVIE_DOWNLOAD("movie:download"),
+    MOVIE_UPLOAD("movie:upload");
+
+    private final String permission;
+
+    ApplicationPermissions(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
