dd51ce77f1174fe01abc7d740dd166a8a8a9cf79
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-05-11 19:21:37 +0430

    MovieResource class before jpa phase

---
+++ src/main/java/ir/hamyiar/security/model/MovieResource.java
@@ -0,0 +1,52 @@
+package ir.hamyiar.security.model;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class MovieResource {
+
+    private UUID id;
+    private String path;
+    private Timestamp createdDateTime;
+    private Timestamp changeDateTime;
+
+
+    public MovieResource(String path, Timestamp createdDateTime, Timestamp changeDateTime) {
+        this.id = UUID.randomUUID();
+        this.path = path;
+        this.createdDateTime = createdDateTime;
+        this.changeDateTime = changeDateTime;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Timestamp getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(Timestamp createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public Timestamp getChangeDateTime() {
+        return changeDateTime;
+    }
+
+    public void setChangeDateTime(Timestamp changeDateTime) {
+        this.changeDateTime = changeDateTime;
+    }
+}
