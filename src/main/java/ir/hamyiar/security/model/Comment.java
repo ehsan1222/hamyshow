c70ea2099216919bfe7251186b0f47c006e5ba10
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-05-11 19:22:24 +0430

    Comment Class before Jpa Phase

---
+++ src/main/java/ir/hamyiar/security/model/Comment.java
@@ -0,0 +1,30 @@
+package ir.hamyiar.security.model;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+
+public class Comment {
+    private final UUID id;
+    private final String message;
+    private final Timestamp createDateTime;
+
+
+    public Comment(String message, Timestamp createDateTime) {
+        this.id = UUID.randomUUID();
+        this.message = message;
+        this.createDateTime = createDateTime;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Timestamp getCreateDateTime() {
+        return createDateTime;
+    }
+}
