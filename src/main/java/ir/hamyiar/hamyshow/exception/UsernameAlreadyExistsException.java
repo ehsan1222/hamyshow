d022c1e4a09dd3890ea84bbbdc346c1868091007
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-05-17 10:05:08 +0430

    UsernameAlreadyExists exception

---
+++ src/main/java/ir/hamyiar/hamyshow/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,16 @@
+package ir.hamyiar.hamyshow.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class UsernameAlreadyExistsException extends RuntimeException {
+
+    public UsernameAlreadyExistsException() {
+
+    }
+
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
