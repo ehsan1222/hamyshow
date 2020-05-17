054aee8dc84cc7f5614f24d351616bccd62a2a18
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-05-18 00:15:18 +0430

    username password userDetailsService

---
+++ src/main/java/ir/hamyiar/hamyshow/security/UsernameAndPasswordUserDetailsService.java
@@ -0,0 +1,14 @@
+package ir.hamyiar.hamyshow.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsernameAndPasswordUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
