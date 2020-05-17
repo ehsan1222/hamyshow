c59338344434531fe5a0be4d5273f46aaa90c5f6
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-05-17 13:21:37 +0430

    user and userInformation repository

---
+++ src/main/java/ir/hamyiar/hamyshow/dao/UserRepository.java
@@ -0,0 +1,18 @@
+package ir.hamyiar.hamyshow.dao;
+
+import ir.hamyiar.hamyshow.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.*;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    @Query("SELECT u FROM User AS u OFFSET :offsetUser LIMIT :limitUser")
+    List<User> getAllUsers(@Param("offsetUser") int offsetUser, @Param("limitUser") int limitUser);
+
+    void deleteByUsername(String username);
+}
