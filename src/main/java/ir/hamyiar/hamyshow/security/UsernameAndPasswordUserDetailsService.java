4c191d7d11b8aaab5f81cbeb1ee442dd89ed601d
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-05-21 00:55:49 +0430

    userDetailsService

--- src/main/java/ir/hamyiar/hamyshow/security/UsernameAndPasswordUserDetailsService.java
+++ src/main/java/ir/hamyiar/hamyshow/security/UsernameAndPasswordUserDetailsService.java
@@ -1,14 +1,49 @@
 package ir.hamyiar.hamyshow.security;
 
+import ir.hamyiar.hamyshow.model.user.User;
+import ir.hamyiar.hamyshow.service.UserService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.Set;
+
 @Service
+@Log4j2
 public class UsernameAndPasswordUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    public UsernameAndPasswordUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        User user = userService.getUserByUsername(username);
+
+        boolean isAccountNonBlock = true;
+        if (user.getIncorrectPasswordFreezeAccount() != null && user.getIncorrectPasswordFreezeAccount().after(new Timestamp(System.currentTimeMillis()))) {
+            isAccountNonBlock = false;
+        }
+        Set<SimpleGrantedAuthority> authorities = user.getUserInformation()
+                .getApplicationUserRole()
+                .getGrantedAuthority();
+        log.info("=-=-=-=-=-=-=-=-=-=-=-0>>>>>>" + user.getUserInformation()
+                .getApplicationUserRole().name());
+        log.info("=-=-=-=-=-=-=-=-=-=-=-0>>>>>>" + user.getUserInformation()
+                .getApplicationUserRole().getGrantedAuthority());
+        return new UsernameAndPasswordUserDetails(
+                authorities,
+                user.getUsername(),
+                user.getPassword(),
+                isAccountNonBlock,
+                true
+        );
     }
 }
