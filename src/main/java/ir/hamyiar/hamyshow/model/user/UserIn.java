03584ea27216a9139ec90dafefc7935c66669fef
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-05-22 01:15:15 +0430

    add password confirm field and no args constructor in userIn

--- src/main/java/ir/hamyiar/hamyshow/model/user/UserIn.java
+++ src/main/java/ir/hamyiar/hamyshow/model/user/UserIn.java
@@ -2,12 +2,15 @@ package ir.hamyiar.hamyshow.model.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserIn {
     private String username;
     private String password;
+    private String passwordConfirm;
     private String fullName;
     private String email;
     private String mobile;
