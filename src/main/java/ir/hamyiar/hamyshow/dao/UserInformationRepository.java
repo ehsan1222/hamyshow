1a97cf143e5feaaa0684eca32c54a76aa0b3eda7
ehsan1222 <s.e.maddahi.ir@gmail.com>
2021-03-19 08:41:31 +0330

    purge legacy project files and configs

--- src/main/java/ir/hamyiar/hamyshow/dao/UserInformationRepository.java
+++
@@ -1,8 +0,0 @@
-package ir.hamyiar.hamyshow.dao;
-
-import ir.hamyiar.hamyshow.model.user.UserInformation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInformationRepository extends JpaRepository<UserInformation, Long> {
-
-}
--- src/main/java/ir/hamyiar/hamyshow/exception/PasswordNotMatchException.java
+++
@@ -1,6 +0,0 @@
-package ir.hamyiar.hamyshow.exception;
-
-public class PasswordNotMatchException extends RuntimeException {
-    public PasswordNotMatchException() {
-    }
-}
